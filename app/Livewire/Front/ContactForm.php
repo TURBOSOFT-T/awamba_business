fb09124576ff0a28cdc12b9070bf39206094f9e2
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2024-11-04 12:59:45 +0100

    update

--- app/Livewire/Front/ContactForm.php
+++ app/Livewire/Front/ContactForm.php
@@ -25,7 +25,7 @@ class ContactForm extends Component
             'nom' => 'required|max:200|string',
             'sujet' => 'required|max:200|string',
             'message' => 'required|max:5000|string',
-         //   'telephone' => 'required',
+            'telephone' => 'required|numeric',
           
         ], [
             'email.required' => 'Veuillez entrer votre email',
@@ -41,7 +41,7 @@ class ContactForm extends Component
         $contact->nom = $this->nom;
         $contact->sujet = $this->sujet;
         $contact->message = $this->message;
-    //    $contact->telephone = $this->telephone;
+        $contact->telephone = $this->telephone;
         if ($contact->save()) {
           
 
@@ -60,7 +60,7 @@ class ContactForm extends Component
                     'nom',
                     'sujet',
                     'message',
-                    'telephone',
+'telephone',
                 
                 ]
             );
