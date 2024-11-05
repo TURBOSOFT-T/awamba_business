4b8f7ce47ca81efabd3da0dd3172b6e0a79824bd
AliMoussa10 <138847373+AliMoussa10@users.noreply.github.com>
2024-11-05 21:17:43 +0100

    Modification de footer

--- resources/views/front/about/about.blade.php
+++ resources/views/front/about/about.blade.php
@@ -6,8 +6,9 @@
     
     $config = DB::table('configs')->first();
 @endphp
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
 
-<div class="axil-breadcrumb-area">
+<div class="axil-breadcrumb-area bg-white">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-lg-6 col-md-8">
@@ -15,131 +16,243 @@
                     <ul class="axil-breadcrumb">
                         <li class="axil-breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
                         <li class="separator"></li>
-                        <li class="axil-breadcrumb-item1 active" aria-current="page">A Propos de Nous</li>
-                        <style>
-                            .axil-breadcrumb-item1 {
-    font-size: 14px;
-    color: #de1212; /* Default breadcrumb color */
-}
-
-.axil-breadcrumb-item.active {
-    font-weight: bold;
-    color: #de1212; /* Distinct color for active item */
-}
-
-                        </style>
+                        <li class="axil-breadcrumb-item1 active" aria-current="page">A Propos</li>
                     </ul>
-                    <h1 class="title">A Propos de notre Boutique</h1>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-4">
-                <div class="inner">
-                    <div class="bradcrumb-thumb">
-                        <img  src="{{ Storage::url($config->image_about ?? ' ') }}" alt="Image">
-                    </div>
-                </div>
-            </div>
+           
         </div>
     </div>
 </div>
 <!-- End Breadcrumb Area  -->
-
-<!-- Start About Area  -->
-<div class="axil-about-area about-style-1 axil-section-gap ">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-xl-4 col-lg-6">
-                <div class="about-thumbnail">
-                    <div class="thumbnail">
-                        <img src="{{ Storage::url($config->image_apropos ?? ' ') }}" alt="About Us">
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-8 col-lg-6">
-                <div class="about-content content-right">
-                 <h4>   <span class="axil-breadcrumb-item1 active" aria-current="page"> <i class="far fa-shopping-basket"></i> A Propos de Nous</span> </h4>
-                   {{--  <span class="title-highlighter highlighter-primary2"> <i class="far fa-shopping-basket"></i>A Propos de nous</span>
-                   --}}  <h3 class="title">{{ $config->titre_apropos ?? '' }}</h3>
-                    
-                    <div class="row">
-                        <div class="col-xl-12">
-                            <p style="text-align: justify">{!! $config->des_apropos ?? ' ' !!}</p>
-                        </div>
-                       
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End About Area  -->
-
-
-<!-- Start About Area  -->
-<div class="axil-about-area about-style-2">
-    <div class="container">
-
-        <div class="row align-items-center">
-            <div class="col-lg-5 order-lg-2">
-                <div class="about-thumbnail">
-                    <img src="{{ Storage::url($config->image_apropos2 ?? ' ') }}"alt="about">
-                </div>
-            </div>
-            <div class="col-lg-7 order-lg-1">
-                <div class="about-content content-left">
-                   
-                    <h4 class="title">{{ $config->titre_apropos2  ?? ' '}}</h4>
-                    <p style="text-align: justify">{!! $config->des_apropos2 ?? ' ' !!}</p>
-                    <a href="{{ route('shop') }}"  class="axil-btn btn-bg-primary2">Voir Boutique</a>
-                </div>
-            </div>
-        </div>
-        <div class="row align-items-center mb--80 mb_sm--60">
-            <div class="col-lg-5">
-                 <div class="about-thumbnail">
-                    <img src="{{ Storage::url($config->image_apropos2 ?? ' ') }}" alt="about">
-                </div> 
-            </div>
-            <div class="col-lg-7">
-                <div class="about-content content-right">
-                   
-                  <h4 class="title">{{ $config->titre_apropos1 ?? ' ' }}</h4>
-                    <p style="text-align: justify">{!! $config->des_apropos1 ?? ' ' !!}</p>
-                    <a href="{{ route('shop') }}" class="axil-btn  btn-bg-primary2 ">Voir Boutique</a>
-                </div>
-            </div>
-        </div>
-       
-    </div>
-</div>
-
-
 <style>
-    .btn-bg-primary2 {
-        background-color: #de1212;
-        color: #ffffff;
-        border: none;
-        padding: 10px 20px;
-        border-radius: 5px;
-        text-decoration: none;
-    }
+.profile {
+	
+            padding: 20px;
+        }
+        
+        .profile .picture {
+            border-radius: 8px;
+            width: 350px;
+            height: 350px;
+            object-fit: cover;
+            margin-bottom: 15px;
+        }
+        
+        .profile h3 {
+			font-size: 1.25rem;
+            margin-bottom: 5px;
+        }
+        
+        .profile p {
+			 color: #555;
+            margin-bottom: 5px;
+        }
 
-    .btn-bg-secondary2 {
-    background-color: #de1212; /* Couleur de fond, bleu dans cet exemple */
-    color: #ffffff; /* Couleur du texte, blanc dans cet exemple */
-    border: none;
-    padding: 10px 20px; /* Optionnel, ajuste la taille */
-    border-radius: 5px; /* Optionnel, arrondit les coins */
-    text-decoration: none; /* Supprime le soulignement */
+       
+
+        /* Service boxes styling */
+        .service-boxs {
+			display:flex;
+			flex-direction:column;
+			justify-content:center;
+			align-items:center;
+            text-align: center;
+            padding: 20px;
+			border:none;
+        }
+        
+        .service-boxs i {
+			margin:0 auto;
+			width:60px;
+			height:60px;
+            font-size: 2.5rem;
+            color: #fff;
+            margin-bottom: 10px;
+			border-radius:50%;
+			border:4px solid #eee;
+			background:#DB4444;
+			padding:15px 13px;
+        }
+        
+        .service-boxs p {
+            margin-bottom: 0;
+        }
+                .stat-box {
+            text-align: center;
+            padding: 20px;
+            border: 1px solid #ddd;
+            transition: background-color 0.3s, color 0.3s;
+            color: black;
+			display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: white;
+            width: 100%; 
+            height: 100%; 
+        }
+
+        .stat-box i {
+			margin:0 auto;
+			width:60px;
+			height:60px;
+            font-size: 3rem;
+            color: #fff;
+            margin-bottom: 10px;
+			border-radius:50%;
+			border:4px solid #eee;
+			background:#DB4444;
+			padding:8px 8px;
+            transition: color 0.3s;
+        }
+		
+		.stat-box .stat-number {
+            font-size: 2em;
+            font-weight: bold;
+        }
+
+        .stat-box:hover {
+            background-color: #DB4444; /* red background on hover */
+            color: white; /* white text on hover */
+			cursor:pointer;
+        }
+
+        .stat-box:hover i,.stat-box:hover p {
+            color: white; /* white icon color on hover */
+        }
+		
+		.row-equal-height .col-md-3 {
+            display: flex;
+            flex-grow: 1;
+        }
+		.social-icons {
+    display: flex; /* Align items in a row */
+	justify-content:start;
+    gap: 10px; /* Space between icons */
 }
-</style>
-<!-- End About Area  -->
 
+.social-icon {
+    width: 20px; /* Set width for the icons */
+    height: auto; /* Maintain aspect ratio */
+    transition: transform 0.2s, opacity 0.2s; /* Transition for hover effects */
+    opacity: 0.8; /* Slightly transparent for aesthetic */
+}
 
-<!-- End Axil Newsletter Area  -->
+.social-icon:hover {
+    transform: scale(1.1); /* Slightly enlarge on hover */
+    opacity: 1; /* Full opacity on hover */
+}
+		
+    </style>
+	<div class="container my-5">
+    <!-- About Section -->
+    <div class="row">
+        <div class="col-12">
+            <h1>Notre Histoire</h1>
+            <p class="text-muted">À propos d’Awamba Business </p>
+            <p>
+               Awamba Business est née de la vision d'une Afrique connectée à l'économie mondiale, où les produits africains trouvent leur place sur la scène internationale. Fondée en décembre 2024, Awamba Business est une plateforme innovante qui permet aux fabricants locaux, artisans, petites entreprises, et commerçants africains de vendre leurs produits non seulement en Afrique mais aussi en Europe, en Amérique et en Asie.
+			</p>
+            <p>
+Awamba Business, c'est avant tout une mission : donner aux producteurs africains les moyens de conquérir de nouveaux marchés et aux consommateurs d'accéder à des produits locaux et internationaux à des prix compétitifs. Notre plateforme se distingue par son engagement envers les vendeurs. Sur Awamba Business, publier des produits est entièrement gratuit, sans abonnement ni frais de commission. Que vous soyez un fabricant local, un grossiste ou un petit commerçant, vous pouvez vendre vos produits à l'international sans frais.
+            </p>
+			<p>
+			Pour les acheteurs, nous offrons également une expérience unique. Toute commande d’un montant minimum de 50 000 francs CFA bénéficie de la livraison gratuite si le produit est localisé dans le même pays que l’acheteur. Pour les produits provenant d'autres pays, nous facilitons les options de livraison internationale.
+			</p>
+			<p>
+			La mission d'Awamba Business
+			</p>
+			<p>
+			Awamba Business a été créée pour transformer l’expérience du commerce africain, en permettant aux produits africains de briller au-delà des frontières. Notre mission se concentre sur trois objectifs clés :
+<ol>
+<li>Connecter les fabricants africains au monde : En permettant aux producteurs locaux de vendre sur les marchés européens, américains et asiatiques, Awamba Business contribue à ouvrir des portes pour l'industrie africaine à l'international.</li>
+<li>Offrir aux commerçants un accès à l’international : Awamba Business permet aux entrepreneurs africains de commander des produits sur des plateformes mondiales comme Amazon et Alibaba et de les revendre sur le marché africain à travers notre plateforme.</li>
+<li>Soutenir le commerce local sans frais : Nous croyons que les créateurs de valeur africaine méritent de prospérer sans frais d’inscription ni commissions, en leur offrant une plateforme où ils peuvent publier et vendre sans contraintes financières.</li>
+</ol>
+			</p>
+			<p>
+			Message du Fondateur – Abdel Awamba
+			</p>
+			<p>
+			Je m'appelle Abdel Awamba, fondateur d’Awamba Business. Mon rêve est de voir les produits africains valorisés à l’international et d'aider les commerçants de notre continent à prospérer. C'est avec cette vision que j'ai lancé Awamba Business, pour qu'elle devienne le tremplin des producteurs et des commerçants africains vers le monde entier. En tant que passionné par l’entrepreneuriat et le potentiel de l'Afrique, je suis déterminé à faire d’Awamba Business une entreprise de référence pour le commerce en ligne en Afrique.
+			</p>
+        </div>
+    </div>
 
+    <!-- Stats Section -->
+    <div class="row row-equal-height text-center mt-5">
+        <div class="col-md-3 mb-3">
+            <div class="stat-box">
+                <i class="bi bi-box-seam fs-1"></i>
+                <p class="stat-number">10.5k</p>
+                <p>Commandes Livrées</p>
+            </div>
+        </div>
+        <div class="col-md-3 mb-3">
+            <div class="stat-box">
+                <i class="bi bi-people fs-1"></i>
+                <p class="stat-number">33k</p>
+                <p>Vendeurs actuellement</p>
+            </div>
+        </div>
+        <div class="col-md-3 mb-3">
+            <div class="stat-box">
+                <i class="bi bi-person-check fs-1"></i>
+                <p class="stat-number">45.5k</p>
+                <p>Clients en ligne</p>
+            </div>
+        </div>
+        <div class="col-md-3 mb-3">
+            <div class="stat-box">
+                <i class="bi bi-cart-check fs-1"></i>
+                <p class="stat-number">25k</p>
+                <p>Commandes en attente</p>
+            </div>
+        </div>
+		<div class="col-md-12 m-3">
+		<!-- Profile Section -->
+    <div class="profile d-flex flex-column justify-content-center align-items-center">
+        <img src="https://t4.ftcdn.net/jpg/06/13/74/95/240_F_613749525_Y1yxH6Ip7e3oJaMvLRYeciBWuXHWMLMV.jpg" class="picture" alt="Profile Picture">
+        
+        <p class="d-flex flex-column align-items-center">
+		<span class="fs-1 fw-bold" >Abdel AWAMBA</span>
+		<span class="w-75 fs-3">Fondateur de la Plateforme AWAMBA BUSINESS</span>
+		<div class="social-icons text-start">
+            <a href="#"><img src="assets/images/icons/twitter.png" class="social-icon" alt="Twitter"></a>
+            <a href="#"><img src="assets/images/icons/instagram.png" class="social-icon" alt="Instagram"></a>
+            <a href="#"><img src="assets/images/icons/sociale.png" class="social-icon" alt="LinkedIn"></a>
+        </div>
+		</p>
+        
+        
+    </div>
+    </div>
+    
+    <!-- Services Section -->
+    <div class="row d-flex justify-content-between mt-5">
+        <div class="col-md-4 mb-3">
+            <div class="service-boxs">
+                <i class="far fa-truck"></i>
+                <h5>Livraison Gratuite et Rapide</h5>
+                <p> 
+				Livraison Gratuite Pour Toute Commande égale
+ou supérieure à 50 000 XAF si vous êtes dans le
+même pays que le fournisseur
+				</p>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="service-boxs">
+                <i class="far fa-headset"></i>
+                <h5>Service Client 24/7</h5>
+                <p> Soutien Client Amical</p>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
 
-<br><br>
 
 
         
--- resources/views/front/fixe.blade.php
+++ resources/views/front/fixe.blade.php
@@ -495,7 +495,7 @@ $produit = DB::table('produits')->get();
     </header> --}}
 
     <a href="#top" class="back-to-top" id="backto-top"><i class="fal fa-arrow-up"></i></a>
-   
+   <div class="page-container">
     <header class="header axil-header header-style-5">
         <div class="axil-header-top">
             <div class="container">
@@ -714,133 +714,20 @@ $produit = DB::table('produits')->get();
     </main>
 
    
-    <!-- Start Footer Area  -->
-    <footer class="axil-footer-area footer-style-2">
-        <!-- Start Footer Top Area  -->
-        <div class="footer-top separator-top">
-            <div class="container">
-                <div class="row">
-                    <!-- Start Single Widget  -->
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">Support</h5>
-                            <!-- <div class="logo mb--30">
-                                <a href="index.html">
-                                    <img class="light-logo" src="assets/images/logo/logo.png" alt="Logo Images">
-                                </a>
-                            </div> -->
-                            <div class="inner">
-                                <p>685 Market Street, <br>
-                                    Las Vegas, LA 95820, <br>
-                                    United States.
-                                </p>
-                                <ul class="support-list-item">
-                                    <li><a href="mailto:example@domain.com"><i class="fal fa-envelope-open"></i> example@domain.com</a></li>
-                                    <li><a href="tel:(+01)850-315-5862"><i class="fal fa-phone-alt"></i> (+01) 850-315-5862</a></li>
-                                    <!-- <li><i class="fal fa-map-marker-alt"></i> 685 Market Street,  <br> Las Vegas, LA 95820, <br> United States.</li> -->
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                    <!-- Start Single Widget  -->
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">Account</h5>
-                            <div class="inner">
-                                <ul>
-                                    <li><a href="my-account.html">My Account</a></li>
-                                    <li><a href="sign-up.html">Login / Register</a></li>
-                                    <li><a href="cart.html">Cart</a></li>
-                                    <li><a href="wishlist.html">Wishlist</a></li>
-                                    <li><a href="shop.html">Shop</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                    <!-- Start Single Widget  -->
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">Quick Link</h5>
-                            <div class="inner">
-                                <ul>
-                                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                                    <li><a href="terms-of-service.html">Terms Of Use</a></li>
-                                    <li><a href="#">FAQ</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                    <!-- Start Single Widget  -->
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">Download App</h5>
-                            <div class="inner">
-                                <span>Save $3 With App & New User only</span>
-                                <div class="download-btn-group">
-                                    <div class="qr-code">
-                                        <img src="assets/images/others/qr.png" alt="Axilthemes">
-                                    </div>
-                                    <div class="app-link">
-                                        <a href="#">
-                                            <img src="assets/images/others/app-store.png" alt="App Store">
-                                        </a>
-                                        <a href="#">
-                                            <img src="assets/images/others/play-store.png" alt="Play Store">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                </div>
-            </div>
-        </div>
-        <!-- End Footer Top Area  -->
-        <!-- Start Copyright Area  -->
-        <div class="copyright-area copyright-default separator-top">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-4">
-                        <div class="social-share">
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                            <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
-                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                            <a href="#"><i class="fab fa-discord"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-12">
-                        <div class="copyright-left d-flex flex-wrap justify-content-center">
-                            <ul class="quick-link">
-                                <li>© 2024. All rights reserved by <a target="_blank" href="https://axilthemes.com/">Axilthemes</a>.</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-12">
-                        <div class="copyright-right d-flex flex-wrap justify-content-xl-end justify-content-center align-items-center">
-                            <span class="card-text">Accept For</span>
-                            <ul class="payment-icons-bottom quick-link">
-                                <li><img src="assets/images/icons/cart/cart-1.png" alt="paypal cart"></li>
-                                <li><img src="assets/images/icons/cart/cart-2.png" alt="paypal cart"></li>
-                                <li><img src="assets/images/icons/cart/cart-5.png" alt="paypal cart"></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Copyright Area  -->
-    </footer>
-    <!-- End Footer Area  -->
-    </div>
+  
+  
+   
 	<style>
-         
+         .page-container {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh; /* Ensure it takes up full viewport height */
+}
+
+main {
+    flex: 1; /* Fills the remaining space, pushing footer down */
+}
+
         .footer {
             background-color: #111;
             color: #fff;
@@ -919,7 +806,7 @@ $produit = DB::table('produits')->get();
     <div class="container-fuild" style="margin-inline:40px 40px">
         <div class="row">
             <!-- Exclusif Section -->
-            <div class="col-md-3">
+            <div class="col-md-3 mb-3">
                 <h6 class="footer-title">Exclusif</h6>
                 <p>S'abonner</p>
                 <p>Bénéficiez de 10€ de réduction sur votre première commande</p>
@@ -933,7 +820,7 @@ $produit = DB::table('produits')->get();
             </div>
 
             <!-- Support Section -->
-            <div class="col-md-3">
+            <div class="col-md-3 mb-3">
                 <h6 class="footer-title">Support</h6>
                 <p>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</p>
                 <p>exclusive@gmail.com</p>
@@ -941,25 +828,25 @@ $produit = DB::table('produits')->get();
             </div>
 
             <!-- Compte Section -->
-            <div class="col-md-2">
+            <div class="col-md-2 mb-3">
                 <h6 class="footer-title">Compte</h6>
                 <a href="#">Mon Compte</a>
-                <a href="#">Se connecter / S'inscrire</a>
+                <a href="/login">Se connecter / S'inscrire</a>
                 <a href="#">Panier</a>
                 <a href="#">Favoris</a>
                 <a href="#">Boutique</a>
             </div>
 
             <!-- Quick Link Section -->
-            <div class="col-md-2">
+            <div class="col-md-2 mb-3">
                 <h6 class="footer-title">Quick Link</h6>
                 <a href="#">Confidentialité</a>
-                <a href="#">Conditions</a>
-                <a href="#">Contacts</a>
+                <a href="/conditions">Conditions</a>
+                <a href="/contact">Contacts</a>
             </div>
 
             <!-- Download App Section -->
-            <div class="col-md-2">
+            <div class="col-md-2 mb-3">
                 <h6 class="footer-title">Download App</h6>
 				 
                 <div class="d-flex gap-4" >
@@ -976,14 +863,16 @@ $produit = DB::table('produits')->get();
                     <a href="#"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
+        
         </div>
 
-        <div class="copy mt-4">
-            &copy; Copyright Rimel 2022. All rights reserved
-        </div>
     </div>
+	<hr class='border-2 border-top mt-4'/>
+	<div class="col-md-12 copy mt-4 ">
+            &copy; Copyright awamba 2024. All rights reserved
+        </div>
 </footer>
-   
+   </div>
     <!-- Product Quick View Modal Start -->
     <div class="modal fade quick-view-product" id="quick-view-modal" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
