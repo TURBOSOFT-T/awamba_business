d2c2ec89a3e19ecc8d60f676297c5f594805612a
AliMoussa10 <138847373+AliMoussa10@users.noreply.github.com>
2024-11-05 12:59:09 +0100

    Modification de footer

--- resources/views/front/fixe.blade.php
+++ resources/views/front/fixe.blade.php
@@ -758,131 +758,151 @@ $produit = DB::table('produits')->get();
             </div>
         </div>
     </div>
-    <!-- Start Footer Area  -->
-    <footer class="axil-footer-area footer-style-2">
-        <!-- Start Footer Top Area  -->
-        <div class="footer-top separator-top">
-            <div class="container">
-                <div class="row">
-                    <!-- Start Single Widget  -->
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">Support</h5>
-                            <!-- <div class="logo mb--30">
-                                <a href="index.html">
-                                    <img class="light-logo" src="assets/images/logo/logo.png" alt="Logo Images">
-                                </a>
-                            </div> -->
-                            <div class="inner">
-                                <p>685 Market Street, <br>
-                                    Las Vegas, LA 95820, <br>
-                                    United States.
-                                </p>
-                                <ul class="support-list-item">
-                                    <li><a href="mailto:example@domain.com"><i class="fal fa-envelope-open"></i> example@domain.com</a></li>
-                                    <li><a href="tel:(+01)850-315-5862"><i class="fal fa-phone-alt"></i> (+01) 850-315-5862</a></li>
-                                    <!-- <li><i class="fal fa-map-marker-alt"></i> 685 Market Street,  <br> Las Vegas, LA 95820, <br> United States.</li> -->
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                    <!-- Start Single Widget  -->
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">Account</h5>
-                            <div class="inner">
-                                <ul>
-                                    <li><a href="my-account.html">My Account</a></li>
-                                    <li><a href="sign-up.html">Login / Register</a></li>
-                                    <li><a href="cart.html">Cart</a></li>
-                                    <li><a href="wishlist.html">Wishlist</a></li>
-                                    <li><a href="shop.html">Shop</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                    <!-- Start Single Widget  -->
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">Quick Link</h5>
-                            <div class="inner">
-                                <ul>
-                                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                                    <li><a href="terms-of-service.html">Terms Of Use</a></li>
-                                    <li><a href="#">FAQ</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                    <!-- Start Single Widget  -->
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">Download App</h5>
-                            <div class="inner">
-                                <span>Save $3 With App & New User only</span>
-                                <div class="download-btn-group">
-                                    <div class="qr-code">
-                                        <img src="assets/images/others/qr.png" alt="Axilthemes">
-                                    </div>
-                                    <div class="app-link">
-                                        <a href="#">
-                                            <img src="assets/images/others/app-store.png" alt="App Store">
-                                        </a>
-                                        <a href="#">
-                                            <img src="assets/images/others/play-store.png" alt="Play Store">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                </div>
-            </div>
-        </div>
-        <!-- End Footer Top Area  -->
-        <!-- Start Copyright Area  -->
-        <div class="copyright-area copyright-default separator-top">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-4">
-                        <div class="social-share">
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                            <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
-                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                            <a href="#"><i class="fab fa-discord"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-12">
-                        <div class="copyright-left d-flex flex-wrap justify-content-center">
-                            <ul class="quick-link">
-                                <li>© 2024. All rights reserved by <a target="_blank" href="https://axilthemes.com/">Axilthemes</a>.</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-12">
-                        <div class="copyright-right d-flex flex-wrap justify-content-xl-end justify-content-center align-items-center">
-                            <span class="card-text">Accept For</span>
-                            <ul class="payment-icons-bottom quick-link">
-                                <li><img src="assets/images/icons/cart/cart-1.png" alt="paypal cart"></li>
-                                <li><img src="assets/images/icons/cart/cart-2.png" alt="paypal cart"></li>
-                                <li><img src="assets/images/icons/cart/cart-5.png" alt="paypal cart"></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Copyright Area  -->
-    </footer>
-    <!-- End Footer Area  -->
+	<style>
+        /* Custom CSS for footer */
+        .footer {
+            background-color: #111;
+            color: #fff;
+            padding: 40px 0;
+            font-size: 14px;
+        }
+        .footer .footer-title, .footer p {
+            font-size: 16px;
+            font-weight: bold;
+			color:#fff;
+            margin-bottom: 15px;
+        }
+        .footer a {
+            color: #fff;
+            text-decoration: none;
+            display: block;
+            margin-bottom: 5px;
+            font-size: 14px;
+        }
+        .footer a:hover {
+            color: #ccc;
+        }
+		.footer .input-group{
+			background-color: #212121;
+            border: 1px solid #ffffff;
+            border-radius: 4px;
+            color: #ffffff;
+            font-size: 14px;
+            padding: 2px 0px;
+            text-align: center;
+            text-decoration: none;
+            display: flex;
+            cursor: pointer;
+		}
+        .footer .input-group .form-control {
+            background-color: transparent;
+            color: #fff;
+            border: none;
+        }
+		.footer .input-group .form-control:focus {
+    outline: none;
+    box-shadow: none;
+    border-color: transparent;
+}
 
+        .footer .input-group-text {
+            background-color: transparent;
+			border:none;
+            color: #fff;
+            cursor: pointer;
+        }
+        .footer .social-icons a {
+            color: #fff;
+            margin-inline: 10px 10px;
+            font-size: 18px;
+        }
+        .footer .app-links a {
+            width: 150px;
+            margin-top: 10px;
+        }
+        .footer .qr-code {
+            width: 100px;
+            margin-top: 10px;
+        }
+        .footer .copy {
+            text-align: center;
+            margin-top: 20px;
+            font-size: 12px;
+            color: #666;
+        }
+		
+	
+    </style>
+	<!-- Footer Section -->
+<footer class="footer">
+    <div class="container-fuild" style="margin-inline:40px 40px">
+        <div class="row">
+            <!-- Exclusif Section -->
+            <div class="col-md-3">
+                <h6 class="footer-title">Exclusif</h6>
+                <p>S'abonner</p>
+                <p>Bénéficiez de 10€ de réduction sur votre première commande</p>
+				 
+                 <div class="input-group w-75">
+                    <input type="email" class="form-control" placeholder="Entrez votre Email">
+                    <span class="input-group-text">
+                        <i class="far fa-send fa-2x"></i>
+                    </span>
+                </div>
+            </div>
+
+            <!-- Support Section -->
+            <div class="col-md-3">
+                <h6 class="footer-title">Support</h6>
+                <p>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</p>
+                <p>exclusive@gmail.com</p>
+                <p>+88015-88888-9999</p>
+            </div>
+
+            <!-- Compte Section -->
+            <div class="col-md-2">
+                <h6 class="footer-title">Compte</h6>
+                <a href="#">Mon Compte</a>
+                <a href="#">Se connecter / S'inscrire</a>
+                <a href="#">Panier</a>
+                <a href="#">Favoris</a>
+                <a href="#">Boutique</a>
+            </div>
+
+            <!-- Quick Link Section -->
+            <div class="col-md-2">
+                <h6 class="footer-title">Quick Link</h6>
+                <a href="#">Confidentialité</a>
+                <a href="#">Conditions</a>
+                <a href="#">Contacts</a>
+            </div>
+
+            <!-- Download App Section -->
+            <div class="col-md-2">
+                <h6 class="footer-title">Download App</h6>
+				 
+                <div class="d-flex gap-4" >
+				<img src="assets/images/others/qr.png" alt="QR Code" class="qr-code">
+                <div class="app-links">
+				<a href="#"><img src="assets/images/others/play-store.png" alt="Google Play"></a>
+                <a href="#"><img src="assets/images/others/app-store.png" alt="App Store"></a>
+                </div>
+				</div>
+                <div class="social-icons d-flex flex-wrap mt-3">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="copy mt-4">
+            &copy; Copyright Rimel 2022. All rights reserved
+        </div>
+    </div>
+</footer>
+   
     <!-- Product Quick View Modal Start -->
     <div class="modal fade quick-view-product" id="quick-view-modal" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
