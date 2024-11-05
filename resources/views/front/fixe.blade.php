56c8d4263e2b2a2e9b0158f689e7dcedc5bb6447
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2024-11-05 14:16:55 +0100

    mise à jour

--- resources/views/front/fixe.blade.php
+++ resources/views/front/fixe.blade.php
@@ -715,142 +715,8 @@ $produit = DB::table('produits')->get();
 
    
   
-    <!-- Start Footer Area  -->
-    <footer class="axil-footer-area footer-style-1 footer-dark">
-        <!-- Start Footer Top Area  -->
-        <div class="footer-top">
-            <div class="container">
-                <div class="row">
-                    <!-- Start Single Widget  -->
-                    <div class="col-md-2 col-sm-6">
-                        
-                            <div class="axil-footer-widget footer-widget-newsletter">
-                                <h4 class="widget-title">Stay Up To Date</h4>
-                                <p>Subscribe to our newsletter and get <span>20% discount</span> offer in your first order</p>
-                                <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Enter Your Email">
-                                    <button class="submit-btn" type="button">SUBSCRIBE</button>
-                                </div>
-                            </div>
-                        
-                    </div>
-
-                    <div class="col-md-2 col-sm-4">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">SUPPORT</h5>
-                            <div class="inner">
-                                <ul>
-                                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                                    <li><a href="terms-of-service.html">Terms Of Use</a></li>
-                                    <li><a href="#">FAQ</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-2 col-sm-4">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">ACCOUNT</h5>
-                            <div class="inner">
-                                <ul>
-                                    <li><a href="my-account.html">My Account</a></li>
-                                    <li><a href="sign-up.html">Login / Register</a></li>
-                                    <li><a href="cart.html">Cart</a></li>
-                                    <li><a href="wishlist.html">Wishlist</a></li>
-                                    <li><a href="shop.html">Shop</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                    <!-- Start Single Widget  -->
-                    <div class="col-md-2 col-sm-12">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">ABOUT</h5>
-                            <div class="inner">
-                                <ul>
-                                    <li><a href="about-us.html">About</a></li>
-                                    <li><a href="about-us.html">Who We Are</a></li>
-                                    <li><a href="blog.html">Latest Post</a></li>
-                                    <li><a href="shop-sidebar.html">New Arraival</a></li>
-                                    <li><a href="contact.html">Contact Us</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-
-                      <!-- Start Single Widget  -->
-                      <div class="col-lg-2 col-sm-12">
-                        <div class="axil-footer-widget">
-                            <h5 class="widget-title">Download App</h5>
-                            <div class="inner">
-                                <span>Save $3 With App & New User only</span>
-                                <div class="download-btn-group">
-                                    <div class="qr-code">
-                                        <img src="assets/images/others/qr.png" alt="Axilthemes">
-                                    </div>
-                                    <div class="app-link">
-                                        <a href="#">
-                                            <img src="assets/images/others/app-store.png" alt="App Store">
-                                        </a>
-                                        <a href="#">
-                                            <img src="assets/images/others/play-store.png" alt="Play Store">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Widget  -->
-                    <!-- Start Single Widget  -->
-              
-                    <!-- End Single Widget  -->
-                    <!-- Start Single Widget  -->
-                 
-                    <!-- End Single Widget  -->
-                </div>
-            </div>
-        </div>
-        <!-- End Footer Top Area  -->
-        <!-- Start Copyright Area  -->
-        <div class="copyright-area copyright-default separator-top">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-4">
-                        {{-- <div class="social-share">
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                            <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
-                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                            <a href="#"><i class="fab fa-discord"></i></a>
-                        </div> --}}
-                    </div>
-                    <div class="col-xl-4 col-lg-12">
-                        <div class="copyright-left d-flex flex-wrap justify-content-center">
-                            <ul class="quick-link">
-                                <li>© 2024. All rights reserved by <a target="_blank" href="https://axilthemes.com/">Axilthemes</a>.</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-12">
-                        <div class="copyright-right d-flex flex-wrap justify-content-xl-end justify-content-center align-items-center">
-                            {{-- <span class="card-text">Accept For</span>
-                            <ul class="payment-icons-bottom quick-link">
-                                <li><img src="assets/images/icons/cart/cart-1.png" alt="paypal cart"></li>
-                                <li><img src="assets/images/icons/cart/cart-2.png" alt="paypal cart"></li>
-                                <li><img src="assets/images/icons/cart/cart-5.png" alt="paypal cart"></li>
-                            </ul> --}}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Copyright Area  -->
-    </footer>
-    <!-- End Footer Area  -->
-    </div>
+  
+   
 	<style>
          
         .footer {
@@ -991,7 +857,7 @@ $produit = DB::table('produits')->get();
         </div>
 
         <div class="copy mt-4">
-            &copy; Copyright Rimel 2022. All rights reserved
+            &copy; Copyright awamba 2024. All rights reserved
         </div>
     </div>
 </footer>
